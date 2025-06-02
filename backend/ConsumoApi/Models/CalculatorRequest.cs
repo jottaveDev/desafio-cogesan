9078f5b23f2b3317752c56c5abe95f0f02b81dee
João Victor <joao_victorflamengo10@outlook.com>
2025-06-02 09:33:39 -0300

    feat: create CalculatorRequest DTO

---
+++ backend/ConsumoApi/Models/CalculatorRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsumoApi.Models
+{
+    public class CalculatorRequest
+    {
+        [Range(1, int.MaxValue, ErrorMessage = "A quantidade de metros deve ser maior que 0.")]
+        public int Meters { get; set; }
+    }
+}
