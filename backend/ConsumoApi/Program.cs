a407411024e5a35c98b39736a386e50745e26873
João Victor <joao_victorflamengo10@outlook.com>
2025-06-02 11:44:09 -0300

    fix: configure cors

--- backend/ConsumoApi/Program.cs
+++ backend/ConsumoApi/Program.cs
@@ -16,6 +16,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(policy =>
+{
+    policy.AllowAnyOrigin()
+          .AllowAnyMethod()
+          .AllowAnyHeader();
+});
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
