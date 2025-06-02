848da5f7c7a70e07064d5cf5e42740c75c7b0ba4
João Victor <joao_victorflamengo10@outlook.com>
2025-06-02 09:51:31 -0300

    refactor: rename vars with more semantic

--- backend/ConsumoApi/Models/Calculator.cs
+++ backend/ConsumoApi/Models/Calculator.cs
@@ -2,35 +2,38 @@ namespace ConsumoApi.Models
 {
     public class Calculator
     {
-        private readonly int meters;
-        private readonly List<Fare> fares;
+        private readonly int totalMeters;
+        private readonly List<Fare> fareBrackets;
 
-        public Calculator(int meters = 0)
+        public Calculator(int totalMeters)
         {
-            this.meters = meters;
-            fares =
+            if (totalMeters <= 0)
+                throw new ArgumentException("A quantidade de metros deve ser maior que zero.", nameof(totalMeters));
+            this.totalMeters = totalMeters;
+            fareBrackets =
             [
-               new() { Limit = 10, Price = 2.0 },
-               new() { Limit = 20, Price = 3.0 },
-               new() { Limit = 30, Price = 5.0 },
-               new() { Limit = int.MaxValue, Price = 8.0 }
+                new() { Limit = 10, Price = 2.0 },
+                new() { Limit = 20, Price = 3.0 },
+                new() { Limit = 30, Price = 5.0 },
+                new() { Limit = int.MaxValue, Price = 8.0 }
             ];
         }
 
         public double CalculatePrice()
         {
-            int remainingMeters = meters;
+            int remainingMeters = totalMeters;
             int calculatedMeters = 0;
             double totalPrice = 0.0;
 
-            foreach (var fare in fares)
+            foreach (var fareBracket in fareBrackets)
             {
                 if (remainingMeters <= 0)
                     break;
 
-                var currentConsumption = Math.Min(remainingMeters, fare.Limit - calculatedMeters);
+                int brackedLimit = fareBracket.Limit - calculatedMeters;
+                var currentConsumption = Math.Min(remainingMeters, brackedLimit);
 
-                totalPrice += currentConsumption * fare.Price;
+                totalPrice += currentConsumption * fareBracket.Price;
                 remainingMeters -= currentConsumption;
                 calculatedMeters += currentConsumption;
             }
